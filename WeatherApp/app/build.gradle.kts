5d5e759bc9bf5d01ef69727611570a609ed5e9f1
Ayat Gamal <ayat.ga106@gmail.com>
2025-03-30 05:40:43 +0200

    security reasons

--- WeatherApp/app/build.gradle.kts
+++ WeatherApp/app/build.gradle.kts
@@ -18,6 +18,12 @@ android {
     val properties = Properties()
     properties.load(FileInputStream(file))
 
+    val localProperties = Properties().apply {
+        load(rootProject.file("local.properties").inputStream())
+    }
+    val mapApiKey: String = localProperties.getProperty("mapApiKey", "")
+
+
     defaultConfig {
         applicationId = "com.example.weatherapp"
         minSdk = 24
@@ -26,7 +32,9 @@ android {
         versionName = "1.0"
 
         buildConfigField("String", "apiKeySafe", properties.getProperty("apiKey"))
-        buildConfigField("String", "mapApiKeySafe", properties.getProperty("mapApiKey"))
+        buildConfigField("String", "mapApiKeySafe", mapApiKey)
+        resValue("string", "mapApiKeySafe", mapApiKey)
+
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
